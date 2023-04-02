20f147fdedb3c0f4ffffa53fe9d1248f95c6a982
IVanEDL <ivanaq3d@gmail.com>
2023-04-02 19:30:46 -0300

    Task 1 fix in comments, Task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,7 +2,9 @@
 """Here I am, documenting a module.
 starts a web flask app that listens on 0.0.0.0 port 5000
 route /: display "Hello HBNB!" 
-route /hbnb: display "HBNB" """
+route /hbnb: display "HBNB" 
+route /c/<text>: display “C ” followed by the value of the text variable
+(replace underscore _ symbols with a space)"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -18,6 +20,13 @@ def hello():
 def HBNB():
     """Returns HBNB"""
     return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """returns c followed by a statement"""
+    return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
