0675443bfb17dafae9d31ba0dc82729fc705d1fc
IVanEDL <ivanaq3d@gmail.com>
2023-04-02 20:33:48 -0300

    last fixes

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -53,9 +53,9 @@ def number_html(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_even(n):
     if n % 2 == 0:
-        string = "even"
+        string = 'even'
     else:
-        string = "odd"
+        string = 'odd'
     return render_template('5-number.html', n=n, string=string)
 
 if __name__ == '__main__':
