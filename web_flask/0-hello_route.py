3dbfc0fe499218290d82e4afd9cb7bcc6ec96777
IVanEDL <ivanaq3d@gmail.com>
2023-04-02 18:55:42 -0300

    Task 0, check for documentation after tests

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""Here I am, documenting a module.
+starts a web flask app that listens on 0.0.0.0 port 5000
+route /: display "Hello HBNB!" """
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,6 +9,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """returns Hello HBNB!"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
